e31efcd3598094a61bb297b58eae342e84c535ee
Rick Peyton <rick.peyton@daveramsey.com>
2017-12-07 08:53:45 -0600

    Seems like the http example is broken

--- http/http-sample.elm
+++ http/http-sample.elm
@@ -3,8 +3,8 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Http
 import Json.Decode as Decode
+import Http
 
 
 main =
